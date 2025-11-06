86342c4f82172d1714d0a58b5fc66661e67d5d21
Hector Manuel Iriarte Ramirez <L25171217@culiacan.tecnm.mx>
2025-11-06 20:52:27 +0000

    agregue nuevo archivo

--- src/Main.java
+++ src/Main.java
@@ -3,15 +3,22 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-        int numero = 0;
+    Scanner sc = new Scanner(System.in);
+    int numero = 0;
+    int suma = 0;
 
         System.out.println("¡Hola mundo desde GitHub Codespaces! 👋");
 
-        while (numero != 5) {
-            System.out.println("ingresa una numero: ");
-         numero = sc.nextInt();
+        while (numero != 7) {
+            System.out.println("Ingresa un número (escribe 7 para salir): ");
+            numero = sc.nextInt();
+            if (numero == 7) {
+                break;
+            }
+            suma += numero;
         }
+
+        System.out.println("La suma total es: " + suma);
 
         sc.close();
     }
